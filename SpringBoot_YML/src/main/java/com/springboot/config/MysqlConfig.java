7034165e88831b8c3eaaa56d80887a7a820b34c8
SKY-20190501WBQ\Administrator <18710971149@163.com>
2020-08-28 19:31:28 +0800

    向容器中添加自定义组件

--- SpringBoot_YML/src/main/java/com/springboot/config/MysqlConfig.java
+++ SpringBoot_YML/src/main/java/com/springboot/config/MysqlConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**给容器中添加组件*/
 @Configuration
 public class MysqlConfig {
 	@Value("${mysql.user-name}")
@@ -18,6 +19,7 @@ public class MysqlConfig {
 	@Value("${mysql.driver-name}")
 	private String driverName;
 	
+	/**将方法的返回值添加到容器中，容器中的这个组件默认的ID就是方法名*/
 	@Bean(name = "mysqlParam")
 	public Map<String, String> mysql(){
 		Map<String, String> map = new HashMap<String, String>();
